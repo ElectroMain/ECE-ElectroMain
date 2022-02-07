9d31d6cf720855096692b6f80526d7a70870c17d
ElectroMain <98825362+ElectroMain@users.noreply.github.com>
2022-02-07 14:55:08 +0100

    Update code_proto.ino

--- code_proto.ino
+++ code_proto.ino
@@ -13,57 +13,61 @@ Servo servoX;
 Servo servoY;
 SoftwareSerial hc05Serial(3, 4); //RX, TX
 
-int movementFlag = 0;
-float speedRatioX = 1;
-float speedRatioY = 0.95;
+//Proto LEGO
+//float speedRatioX = 0.95;
+//float speedRatioY = -0.95;
 
+//Proto 3D
+float speedRatioX = 0.70;
+float speedRatioY = 0.70;
+ 
 coord movements[] = {
-  {80, 130},   // Neutre
-  {90, 180},    // Reculer
+  {90, 90},   // Neutre
+  {90, 180},    // Avancer
+  {90, 0},  // Reculer
   {0, 90},    // Gauche
   {180, 90},  // Droite
-  {90, 0},  // Avancer
+  {0, 180},    // Diagonale NO
+  {180, 180},  // Diagonale NE
+  {0, 0},    // Diagonale SO
+  {180, 0},  // Diagonale SE
 };
 
 void setup() {
-  hc05Serial.begin(9600);
+  hc05Serial.begin(38400);
   servoX.attach(servoPinX);
   servoY.attach(servoPinY);
+  Serial.begin(115200);
 }
 
 void loop() {
-  // Position neutre si fonctionnement commande continu
-  if (movementFlag == 1) {
-    servoX.write(movements[0].x);
-    servoY.write(movements[0].y);
-  }
   
   // Commande recu 
   if (hc05Serial.available()) {
-    // Reset du flag de fonctionnement
-    movementFlag = 0;
     
-    String msg = hc05Serial.readString();
-    int cmd = msg.toInt();
-
+    int cmd = hc05Serial.read();
+    // Convertion ASCII vers décimal
+    if (cmd >= 48 && cmd <= 57) {
+      cmd = cmd - 48;
+    } else {
+      cmd = 0;
+    }
+      
     // Cas fonctionnement commande ponctuelle
-    if (cmd >= 0 && cmd <= 4) {
+    if (cmd >= 0 && cmd <= 8) {
       float x = (movements[cmd].x - 90) * speedRatioX + 90;
       float y = (movements[cmd].y - 90) * speedRatioY + 90;
       servoX.write(x);
       servoY.write(y);
-    }
-    // Cas fonctionnement commande continu
-    else if (cmd >= 5 && cmd <= 8) {
-      movementFlag = 1;
-      servoX.write(movements[cmd-4].x);
-      servoY.write(movements[cmd-4].y);
-    }
-    // Si commande non reconnu => position neutre par sécurité
+    }    // Si commande non reconnu => position neutre par sécurité
     else {
       servoX.write(movements[0].x);
       servoY.write(movements[0].y);
     }
+    
+    
   }
-  delay(100);
+  delay(60);
 }
+
+
